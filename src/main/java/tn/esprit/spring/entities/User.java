ea23249c86650271c0a4b9641e977e5e328b91bb
hsine <hsine.mtiraoui@esprit.tn>
2025-02-08 01:05:31 +0100

    test conflit

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -23,7 +23,8 @@ public class User implements Serializable {
 
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
-	private Long id;   
+	private Long id;
+	//conflit 
 	
 	private String firstName; 
 	
